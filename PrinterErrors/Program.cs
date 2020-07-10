6113ad835c4a6cfa82c7abca10da3faac59b0b87
ChristofBecu <christof.becu@gmail.com>
2020-07-10 17:33:18 +0200

    Solve Kata : Printer Errors
    https://www.codewars.com/kata/56541980fa08ab47a0000040/solutions/csharp/me/newest

---
+++ PrinterErrors/Program.cs
@@ -0,0 +1,50 @@
+﻿// https://www.codewars.com/kata/56541980fa08ab47a0000040/csharp
+// In a factory a printer prints labels for boxes. 
+// For one kind of boxes the printer has to use colors which, 
+// for the sake of simplicity, are named with letters from a to m. 
+
+// The colors used by the printer are recorded in a control string. 
+// For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used 
+// three times color a, four times color b, one time color h then one time color a...
+
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string 
+// is produced e.g.aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+// You have to write a function printer_error which given a string will output the error rate of 
+// the printer as a string representing a rational whose numerator is the number of errors and the 
+// denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+// The string has a length greater or equal to one and contains only letters from ato z.
+
+//#Examples:
+
+// s="aaabbbbhaijjjm"
+// error_printer(s) => "0/14"
+
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// error_printer(s) => "8/22"
+
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace PrinterErrors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string colorString = "aaaxbbbbyyhwawiwjjjwwm";
+            string result = PrinterError(colorString);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        public static string PrinterError(String s)
+        {
+            int errors = Regex.Matches(s.Substring(0), "[n-z#-]").Count;
+            int colors = s.Length;
+            return $"{errors}/{colors}";
+        }
+    }
+}
